1ba0b99fa30db2b8433c81f4b5f2488dc69fc655
seandiddy-cell <seanhinkley21@gmail.com>
2024-10-22 10:19:14 -0400

    finishing signs

--- calculator.c
+++ calculator.c
@@ -1,7 +1,7 @@
 //imports
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <string.h>
 
 //calculator needs
 //add
@@ -33,6 +33,23 @@ int divide(int n1, int n2) {
 	int total = n1 / n2;//divide
 	return total;
 }
+int power(int base, unsigned int exp) {
+    int i, result = 1;
+    for (i = 0; i < exp; i++)
+        result *= base;
+    return result;
+ }
+int askexponent() {
+	int total, pwr, base;
+	//for base
+	printf("Pick base: ");
+	scanf("%d", &base);
+	//for power
+	printf("Pick power: ");
+	scanf("d%", &power);
+	//total = power(base,power);
+	return total;
+}
 
 int numIn() {
 	int num;//declared for scanf
@@ -45,9 +62,9 @@ int numIn() {
 int signIn(int num) {
 	int func;//function number selected
 	int total = num;//total is num for recursion
-	printf("Input function wanted: 0.getTotal 1.add 2.subtract 3.multiply 4.divide:  "); //prints prompt
+	printf("Input function wanted: 0.getTotal 1.add 2.subtract 3.multiply 4.divide 5.exponent(two values) 6.logs:  "); //prints prompt
 	scanf("%d", &func);//scans for integer
-
+	//printf("%d", func);
 	switch(func) {
 	//switch for picking function
 	case 0:
@@ -64,6 +81,10 @@ int signIn(int num) {
 	case 4://division
 		total = divide(total,numIn());
 		break;
+	case 5://exponent
+
+		break;
+
 	}
 	return total;
 }
@@ -75,8 +96,15 @@ void calculatorFunc() {
 	printf("Total: %d", sum);
 }
 
+void getSigns() {
+	printf("pick a math sign: ");
+	char ch[15];
+	scanf("%s", &ch);
+	printf("%s", ch);
+}
 
 int main() {
 	calculatorFunc();
+	//getSigns();
 	return 0;
 }
